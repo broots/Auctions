10d163171cdda3816f2d500b0fab3ba727497f0a
James Brown <brootsb@yahoo.co.uk>
2018-11-20 17:21:08 +0000

    top bid

--- SoftInc.Auctions.Web/Hubs/AuctionHub.cs
+++ SoftInc.Auctions.Web/Hubs/AuctionHub.cs
@@ -32,20 +32,27 @@ namespace SoftInc.Auctions.Web.Hubs
             long bId;
             long itmId;
             decimal amt;
+            decimal? maxAmt = null;
+            var msg = string.Empty;
 
             if (long.TryParse(bidderId, out bId) && long.TryParse(itemId, out itmId) && decimal.TryParse(amount, out amt))
             {
                 var i = Task.Run(async () => await _biddingManager.Search(x => x.ItemId == itmId));
                 var itm = i.Result.OrderByDescending(x => x.Amount).FirstOrDefault();
-                var maxBid = itm?.Amount.GetValueOrDefault();
+                maxAmt = itm?.Amount.GetValueOrDefault();
 
-                var msg = amt > maxBid ? $"Current Max bid {amt:C}" : $"Your bid of {amt:C} below current max bid {maxBid:C}";
+                msg = amt > maxAmt ? $"Current Max bid {amt:C}" : $"Your bid of {amt:C} below current max bid {maxAmt:C}";
+                maxAmt = amt > maxAmt ? amt : maxAmt;
                 //var b = await _
                 var b = Task.Run(async () => await _biddingManager.Save(new Bidding { BidderId = bId, Amount = amt, ItemId = itmId, DateCreated = DateTime.Now, DateModified = DateTime.Now }));
                 var r = b.Result;
-                Clients.All.addNewMessageToPage(name, msg);
+            }
+            else
+            {
+                msg = "Your Bidding session may have expired. Please logout and log back in to continue";
             }
 
+            Clients.All.addNewMessageToPage(name, msg, maxAmt.GetValueOrDefault().ToString("C"));
         }
 
     }
